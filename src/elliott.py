ee77b76a66e59f87a7a2626299fb15fe7d59afde
Samuel Munilla <smunilla@redhat.com>
2018-01-22 14:02:11 -0500

    Refactor flags for subcommands

--- src/elliott.py
+++ src/elliott.py
@@ -5,29 +5,33 @@ from requests_kerberos import HTTPKerberosAuth
 import click
 import requests
 
+# This is a long, multi-parameter URL, there isn't a lot we can do about that
+# pylint: disable=line-too-long
 BUGZILLA_QUERY_URL = 'https://bugzilla.redhat.com/buglist.cgi?bug_status=MODIFIED&classification=Red%20Hat&f1=component&f2=component&f3=component&f4=cf_verified&keywords=UpcomingRelease&keywords_type=nowords&known_name=All%203.x%20MODIFIED%20Bugs&list_id=8111122&o1=notequals&o2=notequals&o3=notequals&o4=notequals&product=OpenShift%20Container%20Platform&query_based_on=All%203.x%20MODIFIED%20Bugs&query_format=advanced&short_desc=%5C%5Bfork%5C%5D&short_desc_type=notregexp&{0}&v1=RFE&v2=Documentation&v3=Security&v4=FailedQA&version=3.0.0&version=3.1.0&version=3.1.1&version=3.2.0&version=3.2.1&version=3.3.0&version=3.3.1&version=3.4.0&version=3.4.1&version=3.5.0&version=3.5.1&version=3.6.0&version=3.6.1&version=3.7.0&version=3.7.1&version=3.8.0&version=3.8.1&version=3.9.0&version=3.9.1&version=3.10.0&version=3.10.1&version=3.11.0&version=3.11.1&version=unspecified'
 
 ERRATA_URL = "https://errata.devel.redhat.com"
-ERRATA_ADD_BUG_URL = ERRATA_URL + '/api/v1/erratum/{}/add_bug'
-ERRATA_BUG_REFRESH_URL = ERRATA_URL + '/api/v1/bug/refresh'
+
+GET_BUILDS_URL = ERRATA_URL + '/api/v1/erratum/{}/builds'
+ADD_BUG_URL = ERRATA_URL + '/api/v1/erratum/{}/add_bug'
+BUG_REFRESH_URL = ERRATA_URL + '/api/v1/bug/refresh'
+
 
 @click.group()
 @click.pass_context
 @click.option("--advisory",
               help="ID for the advisory to operate on")
-@click.option("--target_release",
-              multiple=True,
-              help="Target release versions (e.g. 3.9.x)")
 @click.option('--verbose', '-v', default=False, is_flag=True, help='Enables verbose mode.')
 @click.help_option('--help', '-h')
-def cli(ctx, advisory, target_release, verbose):
+def cli(ctx, advisory, verbose):
     ctx.obj = {}
     ctx.obj['advisory'] = advisory
-    ctx.obj['target_release'] = target_release
     ctx.obj['verbose'] = verbose
 
 
 @cli.command("sweep", help="Add new MODIFED bugs to the advisory")
+@click.option("--target_release",
+              multiple=True,
+              help="Target release versions (e.g. 3.9.x)")
 @click.pass_context
 def sweep(ctx):
     target_releases = ctx.obj['target_release']
@@ -48,19 +52,22 @@ def add_bugs(ctx, bug_list):
     for bug in bug_list:
         click.echo("Adding Bug #{0} to Advisory {1}".format(bug, advisory))
         payload = {'bug': bug}
-        requests.post(ERRATA_ADD_BUG_URL.format(advisory),
+        requests.post(ADD_BUG_URL.format(advisory),
                       auth=HTTPKerberosAuth(),
                       json=payload)
 
 
 @cli.command("find_bugs", help="Find a list of bugs for a specified target release")
+@click.option("--target_release",
+              multiple=True,
+              help="Target release versions (e.g. 3.9.x)")
 @click.pass_context
 def find_bugs(ctx):
     target_releases = ctx.obj['target_release']
 
     click.echo("Searching bugzilla for MODIFIED bugs for release {0}".format(target_releases))
 
-    # target_releases_string="target_release=3.4.z&target_release=3.5.z&target_release=3.6.z"
+    # Example output: "target_release=3.4.z&target_release=3.5.z&target_release=3.6.z"
     target_releases_str = ''
     for release in target_releases:
         target_releases_str += 'target_release={0}&'.format(release)
@@ -88,7 +95,7 @@ def add_flag(ctx, flag, bug_list):
 @click.pass_context
 def refresh_bugs(ctx, bug_list):
     payload = repr(bug_list)
-    requests.post(ERRATA_BUG_REFRESH_URL,
+    requests.post(BUG_REFRESH_URL,
                   auth=HTTPKerberosAuth(), data=payload)
 
 
